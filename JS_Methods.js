892a7c2ea0aac8ce4cb5de2c4ef720ce3dc5e1f5
Lewis Betances <patience174@hotmail.com>
2018-03-27 16:45:20 -0700

    JS Methods

--- JS_Methods.js
+++ JS_Methods.js
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  <script type="text/javascript">
+
 
   // FOREACH METHOD
 
@@ -167,9 +158,3 @@ var arrayForSplit = [];
 arrayForSplit = console.log(string.split(" "));
 //  ["Split, me, into, an, array";]
 
-
-
-
-  </script>
-</body>
-</html>
