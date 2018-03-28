8a8239f540046182fe1dcc7d2c87afd2c0db401c
Lewis Betances <patience174@hotmail.com>
2018-03-28 00:40:53 -0700

    date method

---
+++ dateMethod.js
@@ -0,0 +1,10 @@
+//DATE built in method
+var today = new Date();
+console.log(today.toString());
+console.log(today.getDate());
+console.log(today.getDay());
+
+var someDay = new date('1999/05/10');
+console.log(someDay.toString());
+
+console.log(Date.parse('1999/05/10'));
